48fb878257a959ba7245e80e0c7c2f530a682aae
Giulia De Angeli <deangeli.giulia@iisgalvanimi.edu.it>
2025-02-27 08:48:40 +0000

    sistemazione css

--- services/spotify_oauth.py
+++ services/spotify_oauth.py
@@ -3,7 +3,7 @@ from spotipy.oauth2 import SpotifyOAuth
 
 SPOTIFY_CLIENT_ID = "4a2cbb97c5504b7dbe7a2efc0c840bf2"
 SPOTIFY_CLIENT_SECRET = "0fb3aa1797704da5925b662d34732f95"
-SPOTIFY_REDIRECT_URI = "https://curly-capybara-pjjrvrpv7rxr37gq-5000.app.github.dev/callback" 
+SPOTIFY_REDIRECT_URI = "https://5000-thammahetti-gctbrebuild-2exq3d0cv84.ws-eu118.gitpod.io/callback" 
 
 sp_oauth = SpotifyOAuth(
 client_id=SPOTIFY_CLIENT_ID,
