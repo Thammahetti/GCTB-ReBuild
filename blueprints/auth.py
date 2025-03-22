eb20a6f4bb28fbd234730a938fbdf342ae6f8bb7
TJ <thammahetti.m.t@iisgalvanimi.edu.it>
2025-03-22 23:14:02 +0000

    Modifica bug

--- blueprints/auth.py
+++ blueprints/auth.py
@@ -11,50 +11,7 @@ auth_bp = Blueprint('auth', __name__)
 def register():
     if request.method == 'POST':
         nome = request.form['nome']
-        cognome = request.form['cognome']
-        email = request.form['email']
-        username = request.form['username']
-        password = request.form['password']
-        password_hash = generate_password_hash(password)
-
-        if User.query.filter_by(username=username).first():
-            return render_template('register.html', error="Questo username è già in uso.")
-
-        new_user = User(nome=nome, cognome=cognome, email=email, username=username, password=password_hash)
-        db.session.add(new_user)
-        db.session.commit()
-
-        return redirect(url_for('main_page.login'))
-    return render_template('register.html', error=None)
-
-@main_bp.route('/login', methods=['GET', 'POST'])
-def login():
-    if request.method == 'POST':
-        username = request.form['username']
-        password = request.form['password']
-        user = User.query.filter_by(username=username).first()
-
-        if user and check_password_hash(user.password, password):
-            login_user(user)
-            session['user_id'] = user.id  
-            session['username'] = user.username  
-            return redirect(url_for('main_page.main_home')) 
-        
-        return render_template('login.html', error="Credenziali non valide.")
-    
-    return render_template('login.html', error=None)
-
-
-@auth_bp.route('/login_spotify')
-def login_spotify():
-    auth_url = sp_oauth.get_authorize_url()
-    return redirect(auth_url)  
-
-# Callback 
-@auth_bp.route('/callback')
-def callback():
-    code = request.args.get('code')
-    if code:
+        cognome 
         token_info = sp_oauth.get_access_token(code)
         session['spotify_token'] = token_info
         return redirect(url_for('home.home1'))
