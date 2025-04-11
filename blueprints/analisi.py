5d8c1591755ea3b178f44b3303733d81653cc106
Giulia De Angeli <deangeli.giulia@iisgalvanimi.edu.it>
2025-04-11 15:19:09 +0000

    messo i grafici in fondo alla tabella dei brani

--- blueprints/analisi.py
+++ blueprints/analisi.py
@@ -16,27 +16,33 @@ def brani_playlist():
 
     return render_template('brani_playlist.html', tracks=tracks, playlist_id=playlist_id)
 
+
 @analisi_bp.route('/grafici', methods=['GET'])
 def grafici():
-    """Visualizza tutti i grafici generati per la playlist."""
+    """Visualizza il grafico richiesto per la playlist."""
     playlist_id = request.args.get('playlist_id')
+    graph_type = request.args.get('graph_type')  # Ottieni il tipo di grafico dalla richiesta
+    
     if not playlist_id:
         return "ID della playlist mancante", 400
-    
+
     tracks = get_tracks_from_playlist(playlist_id)
     if not tracks:
         return "Nessun brano trovato", 404
 
-    # Genera i grafici
-    grafico_temporale = generare_grafico_temporale(tracks).to_html(full_html=False)
-    grafico_durata = generare_grafico_durata(tracks).to_html(full_html=False)
-    grafico_popolarita = generare_grafico_popolarita(tracks).to_html(full_html=False)
-    grafico_generi = generare_grafico_generi(tracks).to_html(full_html=False)
-    grafico_evoluzione_popolarita = generare_evoluzione_popolarita(tracks).to_html(full_html=False)
+    # Determina quale grafico generare in base al tipo selezionato
+    if graph_type == "grafico_temporale":
+        graph = generare_grafico_temporale(tracks).to_html(full_html=False)
+    elif graph_type == "grafico_durata":
+        graph = generare_grafico_durata(tracks).to_html(full_html=False)
+    elif graph_type == "grafico_popolarita":
+        graph = generare_grafico_popolarita(tracks).to_html(full_html=False)
+    elif graph_type == "grafico_generi":
+        graph = generare_grafico_generi(tracks).to_html(full_html=False)
+    elif graph_type == "grafico_evoluzione_popolarita":
+        graph = generare_evoluzione_popolarita(tracks).to_html(full_html=False)
+    else:
+        return "Tipo di grafico non valido", 400
 
-    return render_template('grafici.html', 
-                           grafico_temporale=grafico_temporale,
-                           grafico_durata=grafico_durata,
-                           grafico_popolarita=grafico_popolarita,
-                           grafico_generi=grafico_generi,
-                           grafico_evoluzione_popolarita=grafico_evoluzione_popolarita)
+    # Restituisci solo il grafico, non l'intera pagina
+    return {'graph': graph}
